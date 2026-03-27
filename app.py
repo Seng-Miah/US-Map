eb4a0a976c82ebad536b851f2f2fa5722ab921df
Seng-Miah <mogakaj@mail.gvsu.edu>
2026-03-27 11:03:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,31 @@ st.markdown("""
 }
 </style>
 """, unsafe_allow_html=True)
+# =====================================================
+# STATE NAME MAPPING (GLOBAL)
+# =====================================================
+fips_to_state = {
+    "00":"Out of US",
+    "01":"Alabama","02":"Alaska","04":"Arizona","05":"Arkansas","06":"California",
+    "08":"Colorado","09":"Connecticut","10":"Delaware","11":"DC","12":"Florida",
+    "13":"Georgia","15":"Hawaii","16":"Idaho","17":"Illinois","18":"Indiana",
+    "19":"Iowa","20":"Kansas","21":"Kentucky","22":"Louisiana","23":"Maine",
+    "24":"Maryland","25":"Massachusetts","26":"Michigan","27":"Minnesota",
+    "28":"Mississippi","29":"Missouri","30":"Montana","31":"Nebraska",
+    "32":"Nevada","33":"New Hampshire","34":"New Jersey","35":"New Mexico",
+    "36":"New York","37":"North Carolina","38":"North Dakota","39":"Ohio",
+    "40":"Oklahoma","41":"Oregon","42":"Pennsylvania","44":"Rhode Island",
+    "45":"South Carolina","46":"South Dakota","47":"Tennessee","48":"Texas",
+    "49":"Utah","50":"Vermont","51":"Virginia","53":"Washington",
+    "54":"West Virginia","55":"Wisconsin","56":"Wyoming",
 
+    # Territories
+    "60":"American Samoa",
+    "66":"Guam",
+    "69":"Northern Mariana Islands",
+    "72":"Puerto Rico",
+    "78":"Virgin Islands (US)"
+}
 # =====================================================
 # LOAD DATA
 # =====================================================
