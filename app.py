a44fbed3829d3144b9ed9eb2798099130a227087
Seng-Miah <mogakaj@mail.gvsu.edu>
2026-03-31 14:40:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ if section == "National":
             opacity=0.95
         ),
         textposition="top center",
-        textcolor ="black",
+        textcolor="black",
         showlegend=False
     )
 
